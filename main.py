5f153c7d81ed1fdddd7b0c67affb21637decd870
Aka-2021 <aakash.s@flatworldsolutions.in>
2021-11-03 19:03:51 +0530

    first

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import usp
+from usp.tree import sitemap_tree_for_homepage
 
-from selenium import webdriver
+def first_site(url):
+    try:
+        tree = sitemap_tree_for_homepage(url)
+    except:
+        print('error')
 
-driver=webdriver.Chrome('/Users/aakashsaini/Downloads/chromedriver')
-driver.get('https://www.youtube.com/watch?v=-stXyMIrsck')
+    links = []
+    try:
+        for page in tree.all_pages():
+            links.append(page)
+    except:
+        print('error')
+
+    try:
+        for i in links:
+            print(i)
+    except:
+        print('error')
+
+first_site('https://igiaviationdelhi.com')
